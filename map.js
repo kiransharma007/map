bdc2974933b1a8ec3ddbd6b38e8e650100cf9dbc
Kiran <48578218+kiransharma007@users.noreply.github.com>
2019-07-09 23:56:35 +0530

    Update map.js

--- map.js
+++ map.js
@@ -1,6 +1,126 @@
-function changeImage()
+function home()
+{
+var img = document.getElementById("image");
+img.src="bg.jpg";
+return false;
+}
+
+
+function department()
 {
 var img = document.getElementById("image");
 img.src="dept.jpg";
+return false;
+}
+
+function auditorium()
+{
+var img = document.getElementById("image");
+img.src="audi.jpg";
+return false;
+}
+
+function Parking()
+{
+var img = document.getElementById("image");
+img.src="parking.jpg";
+return false;
+}
+
+function bsktblcourt()
+{
+var img = document.getElementById("image");
+img.src="basketball_court.jpg";
+return false;
+}
+
+function bdmntncourt()
+{
+var img = document.getElementById("image");
+img.src="badminton.jpg";
+return false;
+}
+
+function hostel()
+{
+var img = document.getElementById("image");
+img.src="hostels.jpg";
+return false;
+}
+
+function admin()
+{
+var img = document.getElementById("image");
+img.src="admin_block.jpg";
+return false;
+}
+
+function knlgpark()
+{
+var img = document.getElementById("image");
+img.src="knowledge_park.jpg";
+return false;
+}
+
+function gcr()
+{
+var img = document.getElementById("image");
+img.src="gcr.jpg";
+return false;
+}
+
+function lib()
+{
+var img = document.getElementById("image");
+img.src="library.jpg";
+return false;
+}
+
+function cc()
+{
+var img = document.getElementById("image");
+img.src="cc.jpg";
+return false;
+}
+
+function photocpy()
+{
+var img = document.getElementById("image");
+img.src="photocopy_shop.jpg";
+return false;
+}
+
+function finance()
+{
+var img = document.getElementById("image");
+img.src="finance_dept.jpg";
+return false;
+}
+
+function examdiv()
+{
+var img = document.getElementById("image");
+img.src="exam_divison.jpg";
+return false;
+}
+
+function gnd()
+{
+var img = document.getElementById("image");
+img.src="ground.jpg";
+return false;
+}
+
+function tnp()
+{
+var img = document.getElementById("image");
+img.src="tnp.jpg";
+return false;
+}
+
+function canteen()
+{
+var img = document.getElementById("image");
+img.src="canteen.jpg";
 return false;
 }
